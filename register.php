7306febf1453ffe5c6652e4f3dbc02f3f77308d2
isaac Groisman <grisman13@hotmail.com>
2021-03-16 23:58:54 +0200

    register endpoint to users in db success

--- register.php
+++ register.php
@@ -14,21 +14,23 @@
 
     <div class="input-group">
         <label class="text" for="">Username</label>
-        <input type="text" name="username">
+        <input type="text" name="username" value="<?php echo htmlspecialchars($username); ?>">
         <div class="warning"><?php echo $errors['username']; ?></div>
     </div>
     <div class="input-group">
         <label class="text" for="">Email</label>
-        <input type="email" name="email">
+        <input type="email" name="email" value="<?php echo htmlspecialchars($email); ?>">
         <div class="warning"><?php echo $errors['email']; ?></div>
     </div>
     <div class="input-group">
         <label class="text" for="">Password</label>
-        <input type="password" name="password_1">
+        <input type="password" name="password_1" value="<?php echo htmlspecialchars($password_1); ?>">
+        <div class="warning"><?php echo $errors['password_1']; ?></div>
     </div>
     <div class="input-group">
         <label class="text" for="">Confirm Password</label>
-        <input type="password" name="password_2">
+        <input type="password" name="password_2" value="<?php echo htmlspecialchars($password_2); ?>">
+        <div class="warning"><?php echo $errors['password_2']; ?></div>
     </div>
     <div class="input-group">
         <button type="submit" name="register" class="btn">Register</button>
--- server.php
+++ server.php
@@ -2,8 +2,12 @@
 
 $username = '';
 $email = '';
-$errors = array('username' => '', 'email' => '', 'password_1' => '');
+$password_1 = '';
+$password_2 = '';
+
 //$errors = array();
+$errors = array('username' => '', 'email' => '', 'password_1' => '', 'password_2' => '');
+
 
 // connect to the database
 $db = mysqli_connect('localhost', 'root', '', 'pizzagenerator');
@@ -22,7 +26,7 @@ if (isset($_POST['register'])){
     // $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
 
-    // ensure that form fields are filled properly
+    // ensure that form fields are filled properly:
 
     // if(empty($username)){
     //     array_push($errors, "Username is required");
@@ -34,26 +38,53 @@ if (isset($_POST['register'])){
 
     if(empty($_POST['email'])){
         $errors['email'] = "Email is required";
+
+    }  else {
+            $email = $_POST['email'];
+                if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                    $errors['email'] = 'Email must be a valid address';
+                }
+        }
+
+    if(empty($_POST['password_1'])){
+        $errors['password_1'] = "Password is required";
     }
 
-    // if(empty($password_1)){
-    //     array_push($errors, "Password is required");
-    // }
+    if($_POST['password_1'] != $_POST['password_2']){
+        $errors['password_2'] = "The two passwords do not match";
+    }
 
-    // if($password_1 != $password_2){
-    //     array_push($errors, "The two passwords do not match");
-    // }
 
     // if there are no errors, save user to database
-    if (count($errors) == 0){
+    if ($errors['username'] == '' && $errors['email'] == '' && $errors['password_1'] == '' && $errors['password_2'] == '' ){
+    
+        // // encrypt password befor storing in the db
+        // $password = md5($password_1);
+
+        // $sql = "INSERT INTO users (username, email, password)
+        //     VALUES ('$username', '$email', '$password')";
+        
+        // mysqli_query($db, $sql);
+
+
+        // escape sql chars
+        $username = mysqli_real_escape_string($db, $_POST['username']);
+        $email = mysqli_real_escape_string($db, $_POST['email']);
+        $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
 
         // encrypt password befor storing in the db
         $password = md5($password_1);
 
-        $sql = "INSERT INTO users (username, email, password)
-            VALUES ('$username', '$email', '$password')";
-        
-        mysqli_query($db, $sql);
+        // create sql
+        $sql = "INSERT INTO users (username, email, password) VALUES ('$username','$email','$password')";
+
+        // save to db and check
+        if(mysqli_query($db, $sql)){
+            // success
+            header('Location: index.php');
+        } else {
+            echo 'query error: '. mysqli_error($db);
+        }
     }
 
 }
